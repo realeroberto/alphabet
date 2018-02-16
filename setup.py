98db9046032be4deecdea36e998929e3542c3a0d
Roberto Reale <rober.reale@gmail.com>
2018-02-16 11:28:47 +0100

    Create package skeleton.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'alphabet',
+    version = '0.0.1',
+    description = 'A string class enhancement',
+    packages = [ 'alphabet' ],
+    author = 'Roberto Reale',
+    author_email = 'rober.reale@gmail.com',
+    url = 'https://github.com/robertoreale/alphabet',
+    keywords = [ ],
+    install_requires = [ ]
+)
