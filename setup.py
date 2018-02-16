7c7a2fbc0b54244fd514f24547e8c52059b9ed63
Roberto Reale <rober.reale@gmail.com>
2018-02-16 11:59:03 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'alphabet',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'A string class enhancement',
     packages = [ 'alphabet' ],
     author = 'Roberto Reale',
